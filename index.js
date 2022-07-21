0ff52bcb00bfdccc270de3d95d399e2319b7d2b4
Dima <greka16@gmail.com>
2022-07-22 00:48:36 +0300

    merchants

--- index.js
+++ index.js
@@ -35,13 +35,11 @@ $(document).ready(function() {
         // {'link': 'https://secure.wayforpay.com/button/bce62d76ca3f2', 'block': 'page8', 'm': 'site91'},
         // {'link': 'https://secure.wayforpay.com/button/b1a7a9dd9752b', 'block': 'page8', 'm': 'site92'},
         //
-        // {'link': 'https://secure.wayforpay.com/button/b9c82421539bb', 'block': 'page9', 'm': 'site93'},
-        // {'link': 'https://secure.wayforpay.com/button/b9266631b0163', 'block': 'page9', 'm': 'site94'},
+        {'link': 'https://secure.wayforpay.com/button/b9c82421539bb', 'block': 'page9', 'm': 'site93'},
+        {'link': 'https://secure.wayforpay.com/button/b9266631b0163', 'block': 'page9', 'm': 'site94'},
+
         // {'link': 'https://secure.wayforpay.com/button/b977b7833bcad', 'block': 'page9', 'm': 'site95'},
 
-        {'link': 'https://secure.wayforpay.com/button/b48252403d99b', 'block': 'page8', 'm': 'site80'},
-        {'link': 'https://secure.wayforpay.com/button/b82bb064acdec', 'block': 'page8', 'm': 'site81'},
-        {'link': 'https://secure.wayforpay.com/button/b3f8c2c14a416', 'block': 'page8', 'm': 'site82'},
 
 
         // {'link': '', 'block': 'page8', 'm': 'site83'},
